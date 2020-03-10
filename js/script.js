bb50d519e92c4b1ae9816f8be88e6cf411035403
Артур Клювиткин <LeBars@github.com>
2020-03-10 16:56:07 +0500

    Добавление сайта на хост

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let link = document.getElementById("link");
+
+// function color() {
+// 	link.style.color = "red";
+// 	link.style.marginLeft = "10px";
+	
+// }
+// function colorOut() {
+// 	link.style.color = "#fff";
+// }
+
+// link.addEventListener('mouseover', color);
+// link.addEventListener('mouseout', colorOut);
