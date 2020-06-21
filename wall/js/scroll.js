0d658ef9487366a19dbe1d6a90e43b969ad955ca
Артур Клювиткин <LeBars@github.com>
2020-06-21 09:02:03 +0500

    add menu, scroll, adaptive

---
+++ wall/js/scroll.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $(window).on("load", function () {
+        $(".gallery__inner-wrapp").mCustomScrollbar({
+
+            theme: "my-theme",
+            autoHideScrollbar: false,
+            documentTouchScroll: true,
+
+
+        });
+    });
+})(jQuery);
